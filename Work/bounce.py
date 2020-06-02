b16340c7dd509c910938c97d9ec0ca8b0c3fd02e
Godfredgameli <gamezgodfred@gmail.com>
2020-06-02 23:20:58 +0000

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 #meters
+bounce_height = 0.6 #height of each bounce
+num_bounce = 10 #number of bounces
+bounce = 1
+
+while bounce <= num_bounce:
+    height = height * bounce_height
+    print(bounce, round(height, 4))
+    bounce = bounce + 1
+   
