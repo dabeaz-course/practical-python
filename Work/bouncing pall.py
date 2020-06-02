cfd067cde0888440fd219d6511940a74d78c273a
Godfredgameli <gamezgodfred@gmail.com>
2020-06-02 23:18:30 +0000

    Create bouncing pall.py

---
+++ Work/bouncing pall.py
@@ -0,0 +1,12 @@
+#Bouncing ball
+
+height = 100 #meters
+bounce_height = 0.6 #height of each bounce
+num_bounce = 10 #number of bounces
+bounce = 1
+
+while bounce <= num_bounce:
+    height = height * bounce_height
+    print(bounce, round(height, 4))
+    bounce = bounce + 1
+   
