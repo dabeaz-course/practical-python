06d6883be91eec61d2221bbd556c84dcabf46c71
Annemieke-dB <66325357+Annemieke-dB@users.noreply.github.com>
2020-06-06 20:38:31 +0200

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+bounce_height = 100
+bounce_nr = 0
+while bounce_nr < 11:
+  bounce_nr += bounce_nr
+  bounce_height = (3/5)*bounce_height
+  print(bounce_nr,bounce_height)
