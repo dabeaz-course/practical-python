efd3344236e9c64ff4ee07e44ac7bbf7ba5d40c0
sebastianvdn <sebastian.van.den.Noortgaete@gmail.com>
2020-06-21 10:08:25 +0200

    mortage

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,13 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+
+while principal > 0:
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+
+print('Total paid', total_paid)
