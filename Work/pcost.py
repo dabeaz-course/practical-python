6a1ccf96279f73b74d8e19cfc349b5fbea881aea
Niels Moeller-Hansen <moeller84@gmail.com>
2020-06-28 21:56:07 +0200

    finished

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+# with open('Data/portfolio.csv', 'rt') as f:
+#     for line in f:
+#         print(line.split(','))
+total_cost = 0
+
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f).split(',')
+
+for line in f:
+    row = line.split(',')
+    total_cost = total_cost + (int(row[1]) * float(row[2]))
+
+print('Total cost', total_cost)
