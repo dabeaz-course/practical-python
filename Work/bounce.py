71ff6b59247286f28fdb2919d755cdaf1738bf3a
Viktor Pupynin <dec2713@gmail.com>
2020-06-28 15:07:01 +0300

    Exercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+
+# Initial height in meters
+height = 100.0
+for i in range(1,11):
+    height = height * 0.6
+    print(i, round(height,4))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,36 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+# Start pay extra at year 5
+extra_payment_start_month = 12*5
+# Pay extra for 4 years
+extra_payment_end_month = extra_payment_start_month + 12*4
+# Extra payment amount
+extra_payment = 1000
+# Month counter
+current_month = 0
+
+while principal > 0:
+    # Increase month counter
+    current_month += 1
+    # Calculate standard payment
+    principal = principal * (1+rate/12) - payment
+    total_paid += payment
+    # Pay extra if needed
+    if current_month >= extra_payment_start_month and \
+       current_month <= extra_payment_end_month:
+        principal -= extra_payment
+        total_paid += extra_payment
+    # If we've overpaid, return overpaid amount
+    if principal < 0:
+        principal = 0
+        total_paid += principal        
+    print(current_month, round(total_paid,2), round(principal,2))
+
+print('Total paid', round(total_paid, 2))
+print('Months', current_month)
