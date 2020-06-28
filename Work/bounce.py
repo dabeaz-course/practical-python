b562a43bd25bf0f5323e7879a9b9b85cd072a35d
Niels Moeller-Hansen <moeller84@gmail.com>
2020-06-28 20:37:41 +0200

    python introduction

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,18 @@
 # bounce.py
 #
 # Exercise 1.5
+# Exercise 1.5: The Bouncing Ball
+
+# A rubber ball is dropped from a height of 100 meters.
+# Each time it hits the ground, it bounces back up to 3/5 the height it fell.
+# Write a program bounce.py that prints a table showing the height of the first 10 bounces.
+
+# Your program should make a table that looks something like this:
+height = 100
+bounce = 3/5
+bounce_no = 0
+
+while bounce_no < 10:
+    height = height * bounce
+    bounce_no = bounce_no + 1
+    print(round(height, 2), bounce_no)
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,35 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+# Suppose Dave pays an extra $1000/month for the first 12 months of the mortgage?
+#
+# Modify the program to incorporate this extra payment.
+# Have it print the total amount paid along with the number of months required.
+#
+# When you run the new program, it should report a total payment of 929,965.62 over 342 months.
+
+# How much will Dave pay if he pays an extra $1000/month for 4 years starting in year 5 of the mortgage?
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+extra_payment = 1000
+extra_payment_start_month = 60
+extra_payment_end_month = 108
+
+month = 0
+
+while principal > 0:
+    month = month + 1
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    if (month >= extra_payment_start_month) & (month < extra_payment_end_month):
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
+    print( month, total_paid, principal)
+total_paid = total_paid - abs(principal)
+
+print('Total paid', total_paid)
+print('Total months', month)
