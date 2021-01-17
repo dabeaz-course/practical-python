b81fe3c91bf830f659e7b02634c826a3a65cbbd0
Cole Rutledge <ColeRutledge@gmail.com>
2021-01-17 16:30:59 -0500

    adds extra payments

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,9 +6,15 @@ loan_period = 30 * 12   # months
 interest_rate = .05
 monthly_payment = 2684.11
 amt_paid = 0
+pmt_cnt = 0
 
 while mortgage > 0:
-    amt_paid += monthly_payment
-    mortgage = mortgage * (1 + interest_rate / 12) - monthly_payment
+    if pmt_cnt < 12:
+        amt_paid += 1000 + monthly_payment
+        mortgage = mortgage * (1 + interest_rate / 12) - (monthly_payment + 1000)
+    else:
+        amt_paid += monthly_payment
+        mortgage = mortgage * (1 + interest_rate / 12) - monthly_payment
+    pmt_cnt += 1
 
-print(f'left: ${mortgage:.2f}, paid: ${amt_paid:,.2f}')
+print(f'left: ${mortgage:.2f}, paid: ${amt_paid:,.2f}, num_payments: {pmt_cnt}')
