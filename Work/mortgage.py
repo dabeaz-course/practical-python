665472530120fc009a3b1e136c2deb007fe5d2d6
Cole Rutledge <ColeRutledge@gmail.com>
2021-01-17 17:04:59 -0500

    adds correction for overpayment

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -3,7 +3,7 @@
 # Exercise 1.7
 mortgage = 500000       # dollars
 loan_period = 30 * 12   # months
-interest_rate = .05
+interest_rate = .03
 monthly_payment = 2684.11
 amt_paid = 0
 pmt_cnt = 0
@@ -25,5 +25,11 @@ while mortgage > 0:
         amt_paid += monthly_payment
         mortgage = mortgage * (1 + interest_rate / 12) - monthly_payment
     pmt_cnt += 1
+
+    if mortgage < 0:
+        amt_paid += mortgage
+        mortgage = 0
+
+    print(f'{pmt_cnt}\t${amt_paid:,.2f}\t${mortgage:,.2f}')
 
 print(f'left: ${mortgage:.2f}, paid: ${amt_paid:,.2f}, num_payments: {pmt_cnt}')
