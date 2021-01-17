7fe0b3499f47cb25d4ec9a65f5666721591e5ef4
Cole Rutledge <ColeRutledge@gmail.com>
2021-01-17 16:09:53 -0500

    adds exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+cur_height = 100
+bounce_ratio = 3/5
+bounce_cnt = 0
+while bounce_cnt < 10:
+    cur_height *= bounce_ratio
+    bounce_cnt += 1
+    print(bounce_cnt, round(cur_height, 4))
