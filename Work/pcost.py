996b183b5ee4766c6dca65624f7387393d18195b
Cole Rutledge <ColeRutledge@gmail.com>
2021-01-18 10:00:13 -0500

    adds reading a data file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,18 @@
 # pcost.py
 #
 # Exercise 1.27
+from pathlib import Path
+
+path = Path() / 'Work' / 'Data' / 'portfolio.csv'
+
+with open(path, 'rt') as f:
+    hname, hshare, hprice = next(f).split(',')
+    print(f'{hname:^7}| {hshare:^7}| {hprice:^7}')
+
+    # for line in f:
+    #     ticker, shares, price = line.strip().split(',')
+    #     print(f'{ticker:<7}| {shares:<7}| ${price:<7}')
+
+    total_cost = sum(int(line.strip().split(',')[1]) * float(line.strip().split(',')[2])
+                         for line in f)
+    print(f'Total cost: ${total_cost:.2f}')
