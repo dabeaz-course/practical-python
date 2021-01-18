0d2315b9e459009cd7708d0a96da4ee1960b3c17
Cole Rutledge <ColeRutledge@gmail.com>
2021-01-18 10:52:33 -0500

    adds func

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,16 +3,20 @@
 # Exercise 1.27
 from pathlib import Path
 
-path = Path() / 'Work' / 'Data' / 'portfolio.csv'
+def read_file(filepath):
+    with open(filepath, 'rt') as f:
+        # hname, hshare, hprice = next(f).split(',')
+        # print(f'{hname:^7}| {hshare:^7}| {hprice:^7}')
 
-with open(path, 'rt') as f:
-    hname, hshare, hprice = next(f).split(',')
-    print(f'{hname:^7}| {hshare:^7}| {hprice:^7}')
+        # for line in f:
+        #     ticker, shares, price = line.strip().split(',')
+        #     print(f'{ticker:<7}| {shares:<7}| ${price:<7}')
 
-    # for line in f:
-    #     ticker, shares, price = line.strip().split(',')
-    #     print(f'{ticker:<7}| {shares:<7}| ${price:<7}')
+        total_cost = sum(int(l.strip().split(',')[1]) * float(l.strip().split(',')[2])
+                         for l in f
+                         if l.startswith('"'))
+        print(f'Total cost: ${total_cost:.2f}')
 
-    total_cost = sum(int(line.strip().split(',')[1]) * float(line.strip().split(',')[2])
-                         for line in f)
-    print(f'Total cost: ${total_cost:.2f}')
+
+portfolio = Path() / 'Work' / 'Data' / 'portfolio.csv'
+read_file(portfolio)
