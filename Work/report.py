5a534f33708314ff841d7f3479a23f31a627adfe
Cole Rutledge <ColeRutledge@gmail.com>
2021-01-18 16:21:09 -0500

    adds read_portfolio

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,25 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+from rich.pretty import install
+from rich.console import Console
+print = Console(width=100).print
+install(Console(width=100))
+
+
+def read_portfolio(filename):
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        portfolio = []
+
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+
+        return portfolio
+
+portfolio = read_portfolio('Work/Data/portfolio.csv')
