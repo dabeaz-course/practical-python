5d42439fba6ede80b053ab63fa331e65de80a1ee
Cole Rutledge <ColeRutledge@gmail.com>
2021-01-19 12:11:10 -0500

    adds report

--- Work/report.py
+++ Work/report.py
@@ -4,8 +4,9 @@
 
 import csv
 
-from rich.pretty import install
 from rich.console import Console
+from rich.pretty import install
+
 print = Console(width=100).print
 install(Console(width=100))
 
@@ -26,7 +27,6 @@ def read_portfolio(filename):
 
         return portfolio
 
-portfolio = read_portfolio('Work/Data/portfolio.csv')
 
 def read_prices(filename):
     with open(filename, 'rt') as f:
@@ -34,15 +34,32 @@ def read_prices(filename):
                 for row in csv.reader(f)
                 if row}
 
-stock_prices = read_prices('Work/Data/prices.csv')
 
-
-def get_stock_perf(stock):
+def get_stock_perf(stock, mkt_prices):
     bought_price = float(stock['price'])
-    mkt_price = float(stock_prices[stock['name']])
+    current_price = float(mkt_prices[stock['name']])
     shares = int(stock['shares'])
-    return (bought_price - mkt_price) * shares
+    return (bought_price - current_price) * shares
 
 
-gain_loss = sum(get_stock_perf(stock) for stock in portfolio)
-print(f'${gain_loss:,.2f}')
+def make_report(portfolio, mkt_prices):
+    report = []
+    for stock in portfolio:
+        current_price = float(mkt_prices[stock['name']])
+        change = current_price - stock['price']
+        report.append((stock['name'], stock['shares'], current_price, change))
+    return report
+
+
+portfolio = read_portfolio('Work/Data/portfolio.csv')
+mkt_prices = read_prices('Work/Data/prices.csv')
+gain_loss = sum(get_stock_perf(stock, mkt_prices) for stock in portfolio)
+report = make_report(portfolio, mkt_prices)
+
+fields = ('Name', 'Shares', 'Price', 'Change')
+header = ' '.join(f'{field:>10}' for field in fields)
+divider = ' '.join('-' * 10 for _ in range(len(fields)))
+
+print(header, divider, sep='\n')
+for name, shares, price, change in report:
+    print(f"{name:>10s} {shares:10d} {f'${price:.2f}':>10} {change:10.2f}")
