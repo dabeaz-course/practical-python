9bcedf6e32f4d2959a0c69cd6f4f2c7d9d1f8814
Cole Rutledge <ColeRutledge@gmail.com>
2021-01-21 15:20:52 -0500

    adds parse_csv

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,37 @@
 # fileparse.py
 #
 # Exercise 3.3
+import csv
+
+from rich.console import Console
+from rich.pretty import install
+
+print = Console(width=100).print
+install(Console(width=100))
+
+
+def parse_csv(filename: str, select=None) -> list[dict]:
+    '''
+    Parse a CSV file into a list of records
+    '''
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        indices = {headers.index(field) for field in select} if select else None
+        print(indices)
+        records = []
+
+        for row in rows:
+            if not row:
+                continue
+
+            if select:
+                record = {field: row[idx] for field, idx in zip(select, indices)}
+            else:
+                record = dict(zip(headers, row))
+
+            records.append(record)
+    return records
+
+parsed_csv = parse_csv('Work/Data/portfolio.csv', select=['name', 'name'])
+print(parsed_csv)
