af8d42f041e1c4cd6e8ca7e51681c42006e36de5
Cole Rutledge <ColeRutledge@gmail.com>
2021-01-21 18:19:23 -0500

    adds parse_csv final

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -10,28 +10,39 @@ print = Console(width=100).print
 install(Console(width=100))
 
 
-def parse_csv(filename: str, select=None) -> list[dict]:
-    '''
-    Parse a CSV file into a list of records
-    '''
-    with open(filename) as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        indices = {headers.index(field) for field in select} if select else None
-        print(indices)
-        records = []
+def parse_csv(filename: str, *, select=None, types=None,
+              has_headers=True, delimiter=',') -> list[dict]:
 
+    ''' Parse a CSV file into a list of records '''
+    with open(filename) as f:
+        rows = csv.reader(f, delimiter=delimiter)
+        headers = next(rows) if has_headers else []
+
+        if select:
+            indices = [headers.index(field) for field in select]
+            headers = select
+
+        records = []
         for row in rows:
             if not row:
                 continue
 
             if select:
-                record = {field: row[idx] for field, idx in zip(select, indices)}
-            else:
+                row = [row[idx] for idx in indices]
+
+            if types:
+                row = [func(val) for func, val in zip(types, row)]
+
+            if headers:
                 record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
 
             records.append(record)
+
     return records
 
-parsed_csv = parse_csv('Work/Data/portfolio.csv', select=['name', 'name'])
+# parsed_csv = parse_csv('Work/Data/prices.csv', has_headers=False, types=[str, float])
+# parsed_csv = parse_csv('Work/Data/portfolio.dat', types=[str, int, float], delimiter=' ')
+parsed_csv = parse_csv('Work/Data/portfolio.csv', select=['name', 'price', 'shares'], types=[str, float, int])
 print(parsed_csv)
