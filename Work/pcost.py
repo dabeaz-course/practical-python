3c5f10c113adf31f5693a3922dcb0751489bffcd
mrvaita <michele.tassoni@gmail.com>
2021-03-01 14:45:19 +0100

    pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,20 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import os
+
+f_path = "Data/portfolio.csv"
+total_purchase_cost = 0
+
+f = open(f_path, "rt")
+headers = next(f)
+
+for line in f:
+    row = line.split(",")
+    cost = int(row[1]) * float(row[2].strip())
+    total_purchase_cost += cost
+
+f.close()
+
+print(f"Total purchase cost: {total_purchase_cost}")
