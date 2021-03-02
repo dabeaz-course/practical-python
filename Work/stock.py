bfdddf0aed6f9493e71cee4bd2c293742b557305
mrvaita <michele.tassoni@gmail.com>
2021-03-02 15:09:08 +0100

    closures and anonymous functions to support stock class

--- Work/stock.py
+++ Work/stock.py
@@ -1,6 +1,13 @@
+from typedproperty import String, Integer, Float
+
+
 class Stock:
 
-    __slots__ = ("name","_shares", "price")
+    #__slots__ = ("name","_shares", "price")
+
+    name = String("name")
+    shares = Integer("shares")
+    price = Float("price")
 
     def __init__(self, name, shares, price):
 
@@ -11,15 +18,15 @@ class Stock:
     def __repr__(self):
         return f"Stock({self.name}, {self.shares}, {self.price})"
 
-    @property
-    def shares(self):
-        return self._shares
+    #@property
+    #def shares(self):
+    #    return self._shares
 
-    @shares.setter
-    def shares(self, value):
-        if not isinstance(value, int):
-            raise TypeError('Expected int')
-        self._shares = value
+    #@shares.setter
+    #def shares(self, value):
+    #    if not isinstance(value, int):
+    #        raise TypeError('Expected int')
+    #    self._shares = value
 
     @property
     def cost(self):
