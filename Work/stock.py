89294b718f473b3e8434c875252739e660e735f1
mrvaita <michele.tassoni@gmail.com>
2021-03-02 13:38:54 +0100

    Made stock class more consistent

--- Work/stock.py
+++ Work/stock.py
@@ -1,5 +1,7 @@
 class Stock:
 
+    __slots__ = ("name","_shares", "price")
+
     def __init__(self, name, shares, price):
 
         self.name = name
@@ -9,6 +11,17 @@ class Stock:
     def __repr__(self):
         return f"Stock({self.name}, {self.shares}, {self.price})"
 
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected int')
+        self._shares = value
+
+    @property
     def cost(self):
         return self.shares * self.price
 
