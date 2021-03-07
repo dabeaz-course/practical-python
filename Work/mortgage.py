fdd7a5f4de24ca4b798394041f8a63bac1f200b1
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2021-03-07 18:23:33 -0500

    Exercise 1.8 ==> make extra $1000 payment for first 12mos, calculate total cost and time to complete loan

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -5,9 +5,17 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+months = 0
 
+paymentPlus = payment + 1000
+for m in range(1,13):
+    principal = principal * (1 + rate/12) - paymentPlus
+    total_paid += paymentPlus
+    months += 1
 while principal > 0:
     principal = principal * (1 + rate/12) - payment
     total_paid += payment
+    months += 1
 
 print("Total paid: ", round(total_paid, 2))
+print("It took " + str(months) + " months to payoff loan")
