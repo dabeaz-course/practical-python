365b2f0a388274b3d741321028fadc0357728dde
alexgphan <shinyarkeus@gmail.com>
2021-04-03 13:57:05 -0600

    Numbers

--- Work/Numbers.py
+++ Work/Numbers.py
@@ -1,14 +1,19 @@
-
 Mortgage = 500000
 Month = 0
 Interest = float(0.05/12)
 monthlypayment = 2684.11
 total_payed = 0
-while int(Month) < 360: #int(Mortgage) > 0
+
+while int(Month) < 12: #int(Mortgage) > 0
+	Month = Month + 1
+	Mortgage = (float(Mortgage) -  float(monthlypayment)) - 1000
+	Mortgage = float(Mortgage)  + (float(Mortgage) * Interest)
+	total_payed = total_payed + monthlypayment + 1000
+
+while float(Mortgage) > 0: #int(Mortgage) > 0
 	Month = Month + 1
 	Mortgage = (float(Mortgage) -  float(monthlypayment))
 	Mortgage = float(Mortgage)  + (float(Mortgage) * Interest)
-	print(float(Mortgage), int(Month))
 	total_payed = total_payed + monthlypayment
 
 print(float(Month), "Months that passed.")
