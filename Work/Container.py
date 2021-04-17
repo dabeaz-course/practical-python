f5e9885fd81046377fe2f77132aa91946080bec7
alexgphan <shinyarkeus@gmail.com>
2021-04-17 13:57:33 -0600

    2.7

---
+++ Work/Container.py
@@ -0,0 +1,17 @@
+portfolio = [
+	('GOOG', 100, 490.1),
+	('IBM', 50, 91.3),
+	('CAT', 150, 83.44)
+]
+print(portfolio[0])
+print(portfolio[2])
+
+records = []
+
+records.append(('GOOG', 100, 490.10))
+
+records.append(('IBM', 50, 91.3))
+
+print(records)
+
+
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,37 @@
-# report.py
+ # report.py
 #
 # Exercise 2.4
+import csv
+def read_portfolio(filename):
+	list = []
+	with open(filename, 'rt') as f:
+		rows = csv.reader(f)
+		header = next(rows)
+		for row in rows:
+			holding = (row[0], int(row[1]), float(row[2]))
+			list.append(holding)
+	return list
+portfolio = read_portfolio('Data/portfolio.csv')
+total = 0.0
+for s in portfolio:
+	total+= s[1] * s[2]
+print(portfolio)
+from pprint import pprint
+pprint(portfolio)
+
+prices = { }
+prices['IBM'] = 92.45
+prices['MSFT'] = 45.12
+print(prices)
+print(prices['MSFT'])
+
+def read_prices(filename):
+	dict = { }
+	f = open(filename, 'rt')
+	rows = csv.reader(f)
+	for row in rows:
+		try:
+			print(row)
+		except ValueError:
+			print('Not a valid list', row)
+prices = read_prices('Data/prices.csv')
