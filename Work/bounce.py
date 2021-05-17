9c6ae571a772d7f49cda0b6b947a6b843d1915b5
kentokpara <imma.okpara@gmail.com>
2021-05-17 15:26:46 -0230

    okay

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
 # Exercise 1.5
+print("BOUNCE BALL")
+wall = 100 #meters
+bounce = 3/5 #meters
+count = 0
+while count < 10:
+    count+=1
+    ball = wall*bounce
+    wall = ball
+    print(count," ", ball)
+    
+
