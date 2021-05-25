129d8fe709d95c3dcde8d0dc36a28e5ae94dc52d
kentokpara <imma.okpara@gmail.com>
2021-05-25 03:03:32 -0230

    Draft 1

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,13 +1,25 @@
 # mortgage.py
-# Exercise 1.7
+
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+months = 0
+
+extra_payment = 1000.0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
 
 while principal > 0:
+    months = months + 1
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
+    
+    if months >= extra_payment_start_month and months <= extra_payment_end_month:
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
 
-print('Total paid', total_paid)
-
+    print(months, round(total_paid,2), round(principal, 2))
+    
+print('Total paid', round(total_paid, 2))
+print('Months', months)
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open('Work/Data/portfolio.csv','rt') as f :
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        nshares = int(row[1])
+        price = float(row[2])
+        total_cost += nshares * price
+
+print('Total cost', total_cost)
