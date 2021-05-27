a1cdeba0952b9c444621ba6bc5b747d16c2e775d
kentokpara <imma.okpara@gmail.com>
2021-05-27 03:47:55 -0230

    okay

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,6 +2,7 @@
 #
 # Exercise 1.27
 import csv
+import sys
 #filename = 'Work/Data/portfolio.csv'
 def portfolio_cost(filename):
     total_cost =0
@@ -13,12 +14,16 @@ def portfolio_cost(filename):
                 nshares = int(row[1])
                 price = float(row[2])
                 total_cost += nshares * price
-            # This catches errors in int() and float() conversions above
             except ValueError:
                 print('Bad row:', row)
     return total_cost
 
-cost = portfolio_cost('Work/Data/missing.csv')    
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)    
 #portfolio_cost('Data/missing.csv')
 print('Total cost:', cost)
 
