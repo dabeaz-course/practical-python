f290803698328bdc303ec1f7c5b96475cb279f23
kentokpara <imma.okpara@gmail.com>
2021-05-27 03:42:15 -0230

    okay

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,24 @@
 # pcost.py
 #
 # Exercise 1.27
-total_cost =0
-with open('Work/Data/portfolio.csv','rt') as f :
-    headers = next(f)
-    for line in f:
-        row = line.split(',')
-        nshares = int(row[1])
-        price = float(row[2])
-        total_cost += nshares * price
+import csv
+#filename = 'Work/Data/portfolio.csv'
+def portfolio_cost(filename):
+    total_cost =0
+    with open(filename,'rt') as file :
+        rows = csv.reader(file)
+        headers = next(rows)
+        for row in rows:
+            try:
+                nshares = int(row[1])
+                price = float(row[2])
+                total_cost += nshares * price
+            # This catches errors in int() and float() conversions above
+            except ValueError:
+                print('Bad row:', row)
+    return total_cost
 
-print('Total cost', total_cost)
+cost = portfolio_cost('Work/Data/missing.csv')    
+#portfolio_cost('Data/missing.csv')
+print('Total cost:', cost)
+
