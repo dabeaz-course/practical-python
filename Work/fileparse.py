eb1ecabb021b6105c93ffd4dc4214e4e1cf8354a
kentokpara <imma.okpara@gmail.com>
2021-06-10 14:38:42 -0230

    Final -Commit

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,41 +1,47 @@
 # fileparse.py
-#
-# Exercise 3.3
-# fileparse.py
 import csv
 
-def parse_csv(filename, select=None,types=[str, int, float]):
+def parse_csv(lines, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
     '''
-    Parse a CSV file
+    Parse a CSV file into a list of records with type conversion.
     '''
-    with open(filename) as f:
-        rows = csv.reader(f)
+    if select and not has_headers:
+        raise RuntimeError('select column headers')
 
-        # Read the file headers
-        headers = next(rows)
+    rows = csv.reader(lines, delimiter=delimiter)
 
-        # If a column selector was given, find indices of the specified columns.
-        # Also narrow the set of headers used for resulting dictionaries
+    # Read the file headers (if any)
+    headers = next(rows) if has_headers else []
+
+    # If specific columns have been selected, make indices for filtering and set output columns
+    if select:
+        indices = [ headers.index(colname) for colname in select ]
+        headers = select
+
+    records = []
+    for rowno, row in enumerate(rows, 1):
+        if not row:   
+            continue
+
+        # specific column indices are selected, pick them out
         if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
-        else:
-            indices = []
+            row = [ row[index] for index in indices]
 
-        records = []
-        for row in rows:
-            if not row:    # Skip rows with no data
+        # Apply to the row
+        if types:
+            try:
+                row = [func(val) for func, val in zip(types, row)]
+            except ValueError as e:
+                if not silence_errors:
+                    print(f"Row {rowno}: Couldn't convert {row}")
+                    print(f"Row {rowno}: Reason {e}")
                 continue
-            # Filter the row if specific columns were selected
-            if indices:
-                row = [ row[index] for index in indices ]
 
-            # Make a dictionary
+        # Make tuple
+        if headers:
             record = dict(zip(headers, row))
-            records.append(record)
-
-    print(record)
+        else:
+            record = tuple(row)
+        records.append(record)
 
     return records
-
-parse_csv('Work/Data/portfolio.csv')
