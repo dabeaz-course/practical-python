3b387e442efd52c082cfa956c22fba9bc30b2dfe
alexgphan <shinyarkeus@gmail.com>
2021-06-13 13:31:33 -0600

    List

--- Work/collection.py
+++ Work/collection.py
@@ -29,3 +29,24 @@ holding = Counter()
 for s in portfolio2:
 	holding[s['Name']] += s['Shares']
 print(holding)
+
+#nums = [1, 2, 3, 4]
+#squares = [x * x for x in nums]
+#print(squares)
+#double = [2 * x for x in nums if x > 2]
+#print(double)
+
+cost = sum([ s['Shares'] * s['Price'] for s in portfolio ])
+print(cost)
+
+more100 = [s for s in portfolio if s['Shares'] > 100]
+print(more100)
+msftibm = [s for s in portfolio if s['Name'] in {'MSFT', 'IBM'}]
+print(msftibm)
+
+cost10k = [s for s in portfolio if s['Shares'] * s['Price'] > 10000]
+print(cost10k)
+
+name_and_shares = [(s['Name'], s['Shares']) for s in portfolio]
+print(name_and_shares)
+
