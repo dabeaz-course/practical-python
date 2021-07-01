26956e9793356f2a8adf7ab0b99708554ae61c23
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2021-07-01 09:15:03 -0400

    Calculate total cost of stock portfolio by pulling in values from csv

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+totalCost = 0
+with open('Data/portfolio.csv','rt') as f:
+    headers = next(f).split(',')
+    for line in f:
+        row = line.split(',')
+        nshares = int(row[1])
+        price = float(row[2])
+        totalCost += nshares * price
+print(f'Total cost: {totalCost:.2f}')
