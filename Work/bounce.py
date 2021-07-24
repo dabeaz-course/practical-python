0673f4cee5948e08f414d6001a937fd3078a710f
Stanislav Akimov <39214862+akimovstv@users.noreply.github.com>
2021-07-24 14:25:49 +0300

    Add solution for exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,6 @@
-# bounce.py
-#
-# Exercise 1.5
+current_height = 100
+hit_count = 0
+while hit_count < 10:
+    hit_count += 1
+    current_height *= 3 / 5
+    print(hit_count, round(current_height, 4))
