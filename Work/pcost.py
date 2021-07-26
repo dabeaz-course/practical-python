dd0d3c37cff873d1829b817f07586f8529503789
Stanislav Akimov <39214862+akimovstv@users.noreply.github.com>
2021-07-26 08:22:36 +0300

    Add solution for exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,10 @@
-# pcost.py
-#
-# Exercise 1.27
+with open('Data/portfolio.csv') as f:
+    next(f)
+    total_cost = 0.0
+    for line in f:
+        name, shares, price = line.strip().split(',')
+        shares = int(shares)
+        price = float(price)
+        total_cost += shares * price
+
+print(f'Total cost {total_cost}')
