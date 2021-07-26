835087330f91e3c0782d114ed563feaffd119370
Stanislav Akimov <39214862+akimovstv@users.noreply.github.com>
2021-07-26 09:15:19 +0300

    Add solution for exercise 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,18 @@
+import csv
+
+
 def portfolio_cost(filename):
     total_cost = 0.0
     with open(filename) as f:
-        next(f)
-        for line in f:
-            name, shares, price = line.strip().split(',')
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            name, shares, price = row
             try:
                 shares = int(shares)
                 price = float(price)
             except ValueError:
-                print(f'Error occurred while parsing line: {line!r}')
+                print(f'Error occurred while parsing line: {row!r}')
                 continue
             total_cost += shares * price
     return total_cost
