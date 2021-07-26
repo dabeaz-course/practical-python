a604c9caa697c5ba33b6b4df0ae88cd1b3cb35f0
Stanislav Akimov <39214862+akimovstv@users.noreply.github.com>
2021-07-26 16:27:03 +0300

    Add solution for exercise 1.33

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 
 
 def portfolio_cost(filename):
@@ -18,5 +19,10 @@ def portfolio_cost(filename):
     return total_cost
 
 
-cost = portfolio_cost('Data/missing.csv')
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
 print(f'Total cost: {cost}')
