fc2781131c43649ad84c7a993eece1f2b0ee98af
Stanislav Akimov <39214862+akimovstv@users.noreply.github.com>
2021-07-27 11:14:12 +0300

    Add solution for exercise 3.3

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,18 @@
-# fileparse.py
-#
-# Exercise 3.3
+import csv
+
+
+def parse_csv(filename):
+    """
+    Parse a csv-file into a list of records.
+    """
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)  # Read the file headers.
+        records = []
+        for row in rows:
+            if not row:  # Skip rows with no data.
+                continue
+            record = dict(zip(headers, row))
+            records.append(record)
+
+    return records
