031b3d1c4f4ba51e23a4d05c11382562db76df66
Stanislav Akimov <39214862+akimovstv@users.noreply.github.com>
2021-07-27 12:27:43 +0300

    Add solution for exercise 3.6

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -6,23 +6,27 @@ def parse_csv(
         filename: str,
         *,
         select: Optional[Sequence[str]] = None,
-        types: Optional[Sequence[Callable]] = None
+        types: Optional[Sequence[Callable]] = None,
+        has_header: bool = True
 ) -> List[Dict[str, Any]]:
     """
     Parse a csv-file `filename` into a list of records.
     If `select` is provided, then only columns from `select` are added to result records.
     If `types` is provided, then values are converted according to it.
+    If `has_header` is falsy, then `select` is ignored and return result is a list of tuples.
     """
     with open(filename) as f:
         rows = csv.reader(f)
-        headers = next(rows)  # Read the file headers.
 
-        # If a column selector was given, find indices of the specified columns.
-        # Also narrow the set of headers used for resulting dictionaries.
         indices: Optional[List[int]] = None
-        if select is not None:
-            indices = [headers.index(column_name) for column_name in select]
-            headers = list(select)
+        if has_header:
+            headers = next(rows)  # Read the file headers.
+
+            # If a column selector was given, find indices of the specified columns.
+            # Also narrow the set of headers used for resulting dictionaries.
+            if select is not None:
+                indices = [headers.index(column_name) for column_name in select]
+                headers = list(select)
 
         records = []
         for row in rows:
@@ -38,7 +42,10 @@ def parse_csv(
             if types is not None:
                 row = [func(value) for func, value in zip(types, row)]
 
-            record = dict(zip(headers, row))
+            if has_header:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
             records.append(record)
 
     return records
