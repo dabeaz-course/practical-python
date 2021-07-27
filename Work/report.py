8bdd869b998dba89217ec04a70f62b4f01e11cd9
Stanislav Akimov <39214862+akimovstv@users.noreply.github.com>
2021-07-27 10:18:05 +0300

    Add solution for exercise 3.1

--- Work/report.py
+++ Work/report.py
@@ -1,7 +1,11 @@
 import csv
+from typing import Any, Dict, List, Tuple
 
 
-def read_portfolio(filename):
+def read_portfolio(filename: str) -> List[Dict[str, Any]]:
+    """
+    Read a csv-file `filename` to a list of portfolio holdings and return the list.
+    """
     portfolio = []
     with open(filename) as f:
         rows = csv.reader(f)
@@ -18,7 +22,11 @@ def read_portfolio(filename):
         return portfolio
 
 
-def read_prices(filename):
+def read_prices(filename: str) -> Dict[str, float]:
+    """
+    Read a csv-file `filename` of price data into a dict mapping names to prices
+    and return the dict.
+    """
     prices = {}
     with open(filename) as f:
         rows = csv.reader(f)
@@ -30,7 +38,14 @@ def read_prices(filename):
     return prices
 
 
-def make_report(portfolio, prices):
+def make_report(
+        portfolio: List[Dict[str, Any]],
+        prices: Dict[str, float]
+) -> List[Tuple[str, int, float, float]]:
+    """
+    Make a list of (name, shares, price, change) tuples given a portfolio list
+    and prices dictionary.
+    """
     report = []
     for holding in portfolio:
         name = holding['name']
@@ -41,35 +56,46 @@ def make_report(portfolio, prices):
     return report
 
 
+def print_report(report: List[Tuple[str, int, float, float]]) -> None:
+    """
+    Print a nicely formatted table from a list of (name, shares, price, change) tuples.
+    """
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print('{:>10s} {:>10s} {:>10s} {:>10s}'.format(*headers))
+    print('-' * 10, '-' * 10, '-' * 10, '-' * 10)
+    for name, shares, price, change in report:
+        print(f'{name:>10s} {shares:>10d} {"$" + str(round(price, 2)):>10s} {change:>10.2f}')
+
+
+def print_gain(
+        portfolio: List[Dict[str, Any]],
+        prices: Dict[str, float]
+) -> None:
+    """
+    Print whether `portfolio` gain revenue in current `prices`.
+    """
+    start_cost = 0
+    current_cost = 0
+    for holding in portfolio:
+        start_cost += holding['shares'] * holding['price']
+        current_cost += holding['shares'] * prices[holding['name']]
+    print(f'Start cost: {start_cost:.2f}')
+    print(f'Current cost: {current_cost:.2f}')
+    print(f'Gain: {current_cost - start_cost:.2f}')
+
+
 if __name__ == '__main__':
     import sys
 
 
     def main():
-        if len(sys.argv) == 2:
-            filename = sys.argv[1]
-        else:
-            filename = 'Data/portfolio.csv'
-
-        portfolio = read_portfolio(filename)
-        prices = read_prices('Data/prices.csv')
+        portfolio_filename = sys.argv[1] if len(sys.argv) >= 2 else 'Data/portfolio.csv'
+        prices_filename = sys.argv[2] if len(sys.argv) == 3 else 'Data/prices.csv'
+        portfolio = read_portfolio(portfolio_filename)
+        prices = read_prices(prices_filename)
         report = make_report(portfolio, prices)
-
-        headers = ('Name', 'Shares', 'Price', 'Change')
-        print('{:>10s} {:>10s} {:>10s} {:>10s}'.format(*headers))
-        print('-' * 10, '-' * 10, '-' * 10, '-' * 10)
-
-        for name, shares, price, change in report:
-            print(f'{name:>10s} {shares:>10d} {"$" + str(round(price, 2)):>10s} {change:>10.2f}')
-
-        start_cost = 0
-        current_cost = 0
-        for holding in portfolio:
-            start_cost += holding['shares'] * holding['price']
-            current_cost += holding['shares'] * prices[holding['name']]
-        print(f'Start cost: {start_cost:.2f}')
-        print(f'Current cost: {current_cost:.2f}')
-        print(f'Gain: {current_cost - start_cost:.2f}')
+        print_report(report)
+        print_gain(portfolio, prices)
 
 
     main()
