42b54edf11099db7d108f83ff9fd171f2192e35b
Stanislav Akimov <39214862+akimovstv@users.noreply.github.com>
2021-07-30 11:16:39 +0300

    Add solution for exercise 6.3

--- Work/portfolio.py
+++ Work/portfolio.py
@@ -9,11 +9,23 @@ class Portfolio:
         self._holdings = holdings
 
     def __iter__(self) -> Iterator[Stock]:
-        return self._holdings.__iter__()
+        return iter(self._holdings)
+
+    def __len__(self) -> int:
+        return len(self._holdings)
+
+    def __getitem__(self, index: int) -> Stock:
+        return self._holdings[index]
+
+    def __contains__(self, name: str) -> bool:
+        return any(s.name == name for s in self)
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({self._holdings!r})'
 
     @property
     def total_cost(self) -> float:
-        return sum([s.cost for s in self._holdings])
+        return sum([s.cost for s in self])
 
     def tabulate_shares(self) -> Counter:
         total_shares = Counter()
