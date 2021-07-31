687cc29a7be0d4c58406b0fc782fcfaa327017aa
Stanislav Akimov <39214862+akimovstv@users.noreply.github.com>
2021-07-31 08:20:48 +0300

    Improve solution for exercise 7.10

--- Work/timethis.py
+++ Work/timethis.py
@@ -5,9 +5,20 @@ from typing import Callable
 def timethis(func: Callable) -> Callable:
     def wrapper(*args, **kwargs):
         start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        print(f'{func.__module__}.{func.__name__}: {end - start}')
-        return result
+        try:
+            return func(*args, **kwargs)
+        finally:
+            end = time.time()
+            print(f'{func.__module__}.{func.__name__}: {end - start}')
 
     return wrapper
+
+
+if __name__ == '__main__':
+    @timethis
+    def countdown(n: int):
+        while n > 0:
+            n -= 1
+
+
+    countdown(10000000)
