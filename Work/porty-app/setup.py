4e21098f55df97d67d3c344f0c9c6e73dc81ba32
Stanislav Akimov <39214862+akimovstv@users.noreply.github.com>
2021-07-31 18:20:25 +0300

    Add solution for exercise 9.5

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='porty',
+    version='0.0.1',
+    author='Stanislav Akimov',
+    author_email='***',
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+    scripts=['print-report.py'],
+)
