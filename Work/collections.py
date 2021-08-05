528bcdbc6e4d6789310a66c710949ef07df64cde
Alexander Casanova <alexcasan@gmail.com>
2021-08-04 22:35:39 -0400

    Collections section, using Counter() and defaultdict(list)

---
+++ Work/collections.py
@@ -0,0 +1,23 @@
+# Collections
+# Suppose you wanted to tabulate the total number of shares of each stock. 
+# This is easy using #Counter objects. Try it:
+
+portfolio = read_portfolio('Data/portfolio.csv')
+
+from collections import Counter
+
+holdings = Counter()
+
+for value in portfolio:
+	holdings[value['name']] += value['shares']
+
+
+
+portfolio = read_portfolio('Data/portfolio.csv')
+
+from collections import defaultdict
+
+holdings = defaultdict(list)
+
+for value in portfolio:
+	holdings[value['name']].append((value['shares'], value['price']))
