4f95eab5b94a88b529a8621b181c01f3cf11d5ee
root <root@DESKTOP-93NE5FO.localdomain>
2021-08-10 17:38:04 -0400

    Changes from today

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -5,9 +5,10 @@
 import csv
 
 
-def parse_csv(filename: str, select=None, types=list, has_headers=False, delimiter=',') -> list:
+def parse_csv(filename: str, select=None, types=list, has_headers=True, delimiter=',', silence_errors=True) -> list:
     """
     Parses a CSV file with optional types, headers and select vector
+    @filename: 
     """
 
     with open(filename, 'rt') as f:
@@ -17,30 +18,34 @@ def parse_csv(filename: str, select=None, types=list, has_headers=False, delimit
             headers = next(rows)
 
         records = []
-        print(select)
-        print(has_headers)
+        
         if select and has_headers:
             indices = [headers.index(colname) for colname in select]
             headers = select
-        elif not (select and has_headers):
+        elif (select and not has_headers):
         	raise RuntimeError("select argument requires column headers")
         else:
             indices = []
 
-        for row in rows:
-            if not row:  # Skip empty rows.
-                continue
-            if indices:
-                row = [row[index] for index in indices]
-            if types and has_headers:
-                record = {colname: fun(value) for colname,
-                          fun, value in zip(headers, types, row)}
-            if types and not has_headers:
-                record = tuple([(fun(value))
-                               for fun, value in zip(types, row)])
-            else:
-                record = {colname: value for colname,
-                          value in zip(headers, row)}
+        for rowno,row in enumerate(rows):
+            try:
+                if not row:  # Skip empty rows.
+                    continue
+                if indices:
+                    row = [row[index] for index in indices]
+                if types and has_headers:
+                    record = {colname: fun(value) for colname,
+                              fun, value in zip(headers, types, row)}
+                if types and not has_headers:
+                    record = tuple([(fun(value))
+                                   for fun, value in zip(types, row)])
+                else:
+                    record = {colname: value for colname,
+                              value in zip(headers, row)}
+            except ValueError as e:
+                if not silence_errors:
+                    print(f"Row {rowno}: Couldn't convert {row}")
+                    print(f"Row {rowno}: Reason {e}")
 
             records.append(record)
 
@@ -52,4 +57,5 @@ def parse_csv(filename: str, select=None, types=list, has_headers=False, delimit
 """
 #print(parse_csv('Data/prices.csv', select=None, types=[str,float],has_headers=False,delimiter=','))
 #print(parse_csv('Data/portfolio.dat', select=None, types=[str,int,float],has_headers=True,delimiter=' '))
-print(parse_csv('Data/prices.csv', select=None, types=[str,float],has_headers=False,delimiter=','))
+#print(parse_csv('Data/prices.csv', select=None, types=[str,float],has_headers=False,delimiter=','))
+#portfolio = parse_csv('Data/missing.csv', types=[str,int, float], silence_errors=False)
--- Work/report.py
+++ Work/report.py
@@ -1,27 +1,101 @@
-# report.py
+# report_dict.py
 #
 # Exercise 2.4 - example with tuple unpacking and list of tuples
 
 import csv
+import sys
+import fileparse
+from collections import defaultdict
 
-def read_portfolio(filename):
-    '''Computes the total cost (shares*price) of a portfolio file'''
-    portfolio = []
-
-    with open(filename, 'rt') as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
-            portfolio.append(holding)
+def read_portfolio(filename: str) -> list:
+    '''Reads in portfolio and stores into a list of dictionaries.
+       Args: (string) filename ex: 'Data/portfolio.csv' 
+       Return: (dictonary) List of dictonaries.
+    '''
+    portfolio = fileparse.parse_csv(filename)
 
     return portfolio
 
-portfolio = read_portfolio('Data/portfolio.csv')
 
-total = 0.0
+def read_prices(filename: str) -> list:
+    '''Read prices form a file and store as dictionary.'''
 
-for name, shares, price in portfolio:
-    total += shares * price
+    prices = {}
 
-print(total)
+    prices = fileparse.parse_csv('Data/prices.csv', select=None, types=[str,float],has_headers=False,delimiter=',')
+
+    return prices
+
+def make_report(portfolio: list, prices: list) -> list:
+    '''Adjust Portfolio to include change in price and current price.
+
+       Args: portfolio list of dictionaries. Expecting name, shares, price as keys.
+       Return: adj_portolio list of dictionaries, with additional key 'change'
+    '''
+
+    adj_portfolio = []
+
+    for row in portfolio:
+        adj_portfolio.append(({
+            'name':row['name'],
+            'price':row['price'],
+            'shares':row['shares'],
+            'cost': row['price']*row['shares'],
+            'change':prices[row['name']]-row['price']            
+            }))
+
+    return adj_portfolio
+
+def print_report(report:list,portfolio:list,prices:list) -> None:
+    """
+    Print results of the report.
+    Calculates cost and actuals and displays at bottom for profit/loss.
+    """
+
+    cost = sum([row['shares'] * float(row['price']) for row in portfolio])
+    actuals = sum([prices[row['name']]* row['shares'] for row in portfolio])
+
+    headers = ('Name', 'Shares', 'Price', 'Cost', 'Change')
+    print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s} {headers[4]:>10s}')
+    print(f'{"":->10s} {"":->10s} {"":->10s} {"":->10s} {"":->10s}')
+
+    for p in report:
+        price = p['price']
+        p['price'] = f'${price:.2f}'
+        print('{name:>10s} {shares:>10d} {price:>10s} {cost:>10.2f} {change:>10.2f}'.format_map(p))
+
+    print(f'\n')
+    print(f'{"":>10s} {"":>10s} {"Total.Cost":>10s} {cost:>10.2f}')
+    print(f'{"":>10s} {"":>10s} {"Curr.Val":>10s} {cost-actuals:>10.2f}')
+
+    return None
+
+def portfolio_report(portfolio_fname: str, prices_fname: str) -> None:
+    """
+    Executes program to show portfolio_report.
+    """
+    portfolio = read_portfolio(portfolio_fname)
+    prices = read_prices(prices_fname)
+    report = make_report(portfolio, prices)
+    print_report(report,portfolio,prices)
+
+    return None
+
+
+
+if len(sys.argv) > 2:
+    try:
+        portfolio_fname = sys.argv[1]
+        prices_fname = sys.argv[2]    
+
+        portfolio_report(portfolio_fname,prices_fname)
+
+    except IndexError:
+        print(f'Not enough parameters')
+else:
+    portfolio_report('Data/portfolio.csv','Data/prices.csv')
+
+
+
+
+
