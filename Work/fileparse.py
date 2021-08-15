67f29f842dfcd8bd165787463df8b5e2f58d3b50
Alexander Casanova <alexcasan@gmail.com>
2021-08-14 22:46:44 -0400

    Made parse_csv accept iterator instead of a filename so that this function could be more flexible. Raised a SystemExit if lines argument is not a file/iterator.

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,9 +3,10 @@
 # Exercise 3.3
 
 import csv
+import io
 
 
-def parse_csv(filename: str, select=None, types=None, has_headers=True, delimiter=',', silence_errors=True) -> list:
+def parse_csv(lines: list, select=None, types=None, has_headers=True, delimiter=',', silence_errors=True) -> list:
     """
     Parses a CSV file with optional types, headers and select vector
 
@@ -22,51 +23,44 @@ def parse_csv(filename: str, select=None, types=None, has_headers=True, delimite
 
     """
 
-    with open(filename, 'rt') as f:
+    if not isinstance(lines, (io.TextIOBase, list)):
+        raise SystemExit(f'Usage: lines argument is not a file: {lines}')
 
-        rows = csv.reader(f, delimiter=delimiter)
-        if has_headers:
-            headers = next(rows)
+    rows = csv.reader(lines, delimiter=delimiter)
 
-        records = []
-        
-        if select and has_headers:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
-        elif (select and not has_headers):
-        	raise RuntimeError("select argument requires column headers")
-        else:
-            indices = []
+    if has_headers:
+        headers = next(rows)
 
-        for rowno,row in enumerate(rows):
+    records = []
+    
+    if select and has_headers:
+        indices = [headers.index(colname) for colname in select]
+        headers = select
+    elif (select and not has_headers):
+    	raise RuntimeError("select argument requires column headers")
+    else:
+        indices = []
 
-            try:
-                if not row:  # Skip empty rows.
-                    continue
-                if indices:
-                    row = [row[index] for index in indices]
+    for rowno,row in enumerate(rows):
 
-                if types and has_headers:
-                    records.append(({colname: fun(value) for colname,
-                              fun, value in zip(headers, types, row)}))  
-                elif types and not has_headers:
-                    records.append(tuple([(fun(value))
-                                   for fun, value in zip(types, row)]))
-                elif not types and has_headers:                    
-                    records.append(({colname: value for colname,
-                              value in zip(headers, row)}))                
+        try:
+            if not row:  # Skip empty rows.
+                continue
+            if indices:
+                row = [row[index] for index in indices]
 
-            except ValueError as e:
-                if not silence_errors:
-                    print(f"Row {rowno}: Couldn't convert {row}")
-                    print(f"Row {rowno}: Reason {e}")
-        return records
+            if types and has_headers:
+                records.append(({colname: fun(value) for colname,
+                          fun, value in zip(headers, types, row)}))  
+            elif types and not has_headers:
+                records.append(tuple([(fun(value))
+                               for fun, value in zip(types, row)]))
+            elif not types and has_headers:                    
+                records.append(({colname: value for colname,
+                          value in zip(headers, row)}))                
 
-
-"""print(parse_csv('Data/portfolio.csv',
-      select=['name', 'shares', 'price'], types=[str, int, float], has_headers=True))
-"""
-#print(parse_csv('Data/prices.csv', select=None, types=[str,float],has_headers=False,delimiter=','))
-#print(parse_csv('Data/portfolio.dat', select=None, types=[str,int,float],has_headers=True,delimiter=' '))
-#print(parse_csv('Data/prices.csv', select=None, types=[str,float],has_headers=False,delimiter=','))
-#portfolio = parse_csv('Data/missing.csv', types=[str,int, float], silence_errors=False)
+        except ValueError as e:
+            if not silence_errors:
+                print(f"Row {rowno}: Couldn't convert {row}")
+                print(f"Row {rowno}: Reason {e}")
+    return records
