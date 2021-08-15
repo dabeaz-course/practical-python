24ba2074bb51c0f79dc51b42a7f08f593a69ee18
Alexander Casanova <alexcasan@gmail.com>
2021-08-14 22:48:44 -0400

    Added ability for the file to be called via bash script by adding #! and making file executable using chmod +x. Also added main() function.

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,19 +1,40 @@
-# pcost.py
-#
+#!/usr/bin/env python3
+#pcost.py
+
 # Exercise 1.27
+# Modified to use csv imported library.
+# Handles csv files natively, gets rid of double quotes
+# No need to split by delimitter.
+
+# run with python3 -i pcost_csv.py
+# inputs: portfolio_cost('Data/portfolio.csv') 
+#         portfolio_cost('Data/missing.csv')    <-- uses try, except catch.
+
+import csv
+import report
 
 def portfolio_cost(filename):
-    with open(filename,'rt') as file:
-        header = next(file)
-        data = []
-        cost = 0
-        for line in file:
-            data = line.split(',')
-            try:
-                shares = int(data[1])
-                cost += shares * float(data[2])
-            except ValueError:
-                print(f'This is an improper line: {data}')
-        return cost
+    'Using csv library to parse csv.'
+    portfolio = report.read_portfolio(filename)
 
-cost = portfolio_cost(filename)
+    total_cost = 0
+    for row in portfolio:
+        total_cost += row['shares'] * row['price']  
+
+    return total_cost
+
+
+def main(argv):
+    # Main function
+
+    if len(argv) != 2:
+        raise SystemExit(f'Usage: {argv[0]} ','filename')
+
+    filename = argv[1]
+    cost = portfolio_cost(filename=filename)
+    print('Total Cost:', cost)
+
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
