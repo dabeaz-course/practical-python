73f311f5fda89c2ce06c85e04c3e2f7f59f1a28c
dekusuke <iylian9@naver.com>
2021-09-14 12:19:00 +0900

    change
    Signed-off-by: dekusuke <iylian9@naver.com>

--- Solutions/1_10/mortgage.py
+++ Solutions/1_10/mortgage.py
@@ -7,7 +7,7 @@ total_paid = 0.0
 month = 0
 
 extra_payment = 1000.0
-extra_payment_start_month = 61
+extra_payment_start_month = 60
 extra_payment_end_month = 108
 
 while principal > 0:
@@ -15,14 +15,19 @@ while principal > 0:
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
 
-    if month >= extra_payment_start_month and month <= extra_payment_end_month:
+    if month > extra_payment_start_month and month <= extra_payment_end_month:
         principal = principal - extra_payment
         total_paid = total_paid + extra_payment
 
     print(month, round(total_paid,2), round(principal, 2))
+if principal < 0:
+    total_paid = total_paid + principal
+    principal = 0
+    
     
 print('Total paid', round(total_paid, 2))
-print('Months', month)
+print('Months', month + 1)
 
 
 
+ 
