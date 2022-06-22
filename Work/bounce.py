0f8f1536cb29dc563fbb66c3e6ed11a3a88086a9
Tal Ghenassia <85259629+talghe@users.noreply.github.com>
2022-06-22 15:11:38 +0300

    bounce.py completed

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+starting_height = 100 #meters
+actual_height = starting_height #meters
+nb_bounces = 10
+
+while nb_bounces > 0 :
+    actual_height = (actual_height*3)/5
+    print(actual_height)
+    nb_bounces = nb_bounces -1
