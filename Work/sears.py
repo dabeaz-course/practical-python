d57333c3d53d48be662b4c27f0bb199a4b49bc80
Tal Ghenassia <85259629+talghe@users.noreply.github.com>
2022-06-23 17:24:42 +0300

    update from 23/06/2022 at 17:24

--- Work/sears.py
+++ Work/sears.py
@@ -7,7 +7,7 @@ day            = 1
 
 while num_bills * bill_thickness < sears_height:
     print(day, num_bills, num_bills * bill_thickness)
-    day = days + 1
+    day = day + 1
     num_bills = num_bills * 2
 
 print('Number of days', day)
