214d63d843dc3640ec59dd2feaec7cba0d56fc99
Tal Ghenassia <85259629+talghe@users.noreply.github.com>
2022-06-27 11:55:03 +0300

    Update 11:55

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,12 +2,14 @@
 #
 # Exercise 1.27
 
-cost = 0
+total_cost = 0
 
 with open('Data/portfolio.csv', 'rt') as f:
     next(f)
     for line in f:
         row = line.split(',')
-        cost += int(row[1])*float(row[2])
+        share = int(row[1])
+        cost = float(row[2])
+        total_cost += share*cost
 
-print('Total cost ', cost)
+print('Total cost ', total_cost)
