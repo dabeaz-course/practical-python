2a6deb86531d83e7a1dd1aaf5777d5df0155ce3c
Tal Ghenassia <85259629+talghe@users.noreply.github.com>
2022-06-27 10:43:50 +0300

    Adding pcost.py file - operational

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+
+cost = 0
+
+with open('Data/portfolio.csv', 'rt') as f:
+    next(f)
+    for line in f:
+        row = line.split(',')
+        cost += int(row[1])*float(row[2])
+
+print('Total cost ', cost)
