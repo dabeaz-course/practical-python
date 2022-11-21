1dd909c87f572d6cd34cfd4a5671befa001988f4
MBLVRD <badmanskiph@gmail.com>
2022-11-21 16:49:45 +0300

    Задание 1.27. Как делишки?

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,10 @@
 # pcost.py
 #
 # Exercise 1.27
+with open('Data/portfolio.csv', 'rt') as f:
+    name, shares, price = next(f).split(',')
+    Total_cost = 0.00
+    for lines in f:
+        name, shares, price = lines.split(',')
+        Total_cost += int(shares) * float(price)
+    print('Total cost', Total_cost)
