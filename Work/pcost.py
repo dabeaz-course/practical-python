bf7d1b748e9917ccbf817cd88189b8bac782e5aa
MBLVRD <badmanskiph@gmail.com>
2022-11-21 17:30:00 +0300

    Увлёкся, 1.30 сделал вместе с 1.31 + модифицировал

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,10 +1,24 @@
 # pcost.py
 #
-# Exercise 1.27
-with open('Data/portfolio.csv', 'rt') as f:
-    name, shares, price = next(f).split(',')
-    Total_cost = 0.00
-    for lines in f:
-        name, shares, price = lines.split(',')
-        Total_cost += int(shares) * float(price)
-    print('Total cost', Total_cost)
+# Exercise 1.27 + 1.30 + 1.31
+def portfolio_cost(filename):
+    counterOfLines = 0
+    try:
+        with open(filename, 'rt') as f:
+            name, shares, price = next(f).split(',')
+            counterOfLines += 1
+            Total_cost = 0.00
+            for lines in f:
+                try:
+                    name, shares, price = lines.split(',')
+                    Total_cost += int(shares) * float(price)
+                    counterOfLines += 1
+                    print('Промежуточный итог:', Total_cost)
+                except ValueError:
+                    print('Проблема со строкой', counterOfLines, ': не хватает информации')
+                    pass    
+        return Total_cost
+    except FileNotFoundError:
+        print('Ну ты чего, братишка? Такого файла нет!')
+cost = portfolio_cost('Data/portfolioTest.csv')
+print('Total cost:', cost)
