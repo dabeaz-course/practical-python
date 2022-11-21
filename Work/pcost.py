87c6a6f8aa67abead635a0c8dd6c3175d825b75c
MBLVRD <badmanskiph@gmail.com>
2022-11-21 17:57:19 +0300

    Так, это по заданию 1.32 Надеюсь, что видно норм

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,24 +1,24 @@
 # pcost.py
 #
-# Exercise 1.27 + 1.30 + 1.31
+# Exercise 1.27 + 1.30 + 1.31 + 1.32
 def portfolio_cost(filename):
+    import csv
     counterOfLines = 0
+    Total_cost = 0.00
     try:
-        with open(filename, 'rt') as f:
-            name, shares, price = next(f).split(',')
-            counterOfLines += 1
-            Total_cost = 0.00
-            for lines in f:
+        with open(filename) as f:
+            rows = csv.reader(f)
+            headers = next(rows)
+            for row in rows:
                 try:
-                    name, shares, price = lines.split(',')
-                    Total_cost += int(shares) * float(price)
                     counterOfLines += 1
-                    print('Промежуточный итог:', Total_cost)
-                except ValueError:
+                    Total_cost += int(row[1]) * float(row[2])
+                except IndexError:
                     print('Проблема со строкой', counterOfLines, ': не хватает информации')
                     pass    
         return Total_cost
     except FileNotFoundError:
         print('Ну ты чего, братишка? Такого файла нет!')
+
 cost = portfolio_cost('Data/portfolioTest.csv')
 print('Total cost:', cost)
