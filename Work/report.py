3e0bf85f407587082f10071d9bbd647a804bdca0
MBLVRD <badmanskiph@gmail.com>
2022-11-24 18:20:07 +0300

    2.4 + 2.5

--- Work/report.py
+++ Work/report.py
@@ -2,7 +2,8 @@
 #
 # Exercise 2.4
 import csv
-def read_portfolio(filename):
+
+def read_portfolio_tuples(filename):
     portfolio = []
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
@@ -11,6 +12,18 @@ def read_portfolio(filename):
             temp = (row[0], int(row[1]), float(row[2]))
             portfolio.append(temp)
     return portfolio
+
+def read_portfolio_dict(filename):
+    portfolio = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            temp = {'name' : row[0], 'shares': int(row[1]), 'price': float(row[2])}
+            portfolio.append(temp)
+    return portfolio
     
-portfolio = read_portfolio('Data/portfolio.csv')
+portfolio = read_portfolio_dict('Data/portfolio.csv')
+print(portfolio)
+portfolio = read_portfolio_tuples('Data/portfolio.csv')
 print(portfolio)
