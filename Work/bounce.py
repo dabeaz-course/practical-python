ced96a56b6a4b91e0f4e453bb887ac21fb52e07b
trstevens <tylerr.stevens12@gmail.com>
2023-06-08 12:20:49 -0500

    Code for bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bounce = 3/5
+
+for i in range(1,11):
+    new_height = round(height * bounce, 4) 
+    print(f'{i} {new_height}')
+    height = new_height
+
