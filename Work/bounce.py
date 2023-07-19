604a0625fa0bf69b8d058b3f494ce02ee318ac12
srschwartz <srschwartz@utexas.edu>
2023-07-19 17:52:57 -0500

    ex 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -2,10 +2,9 @@
 #
 # Exercise 1.5
 height = 100
-bounce = 1
+bounce = 0
 
-while bounce <= 10:
-    print(bounce, height)
+while bounce < 10:
     bounce += 1
     height = (height - (height * 0.4))
-    
+    print(bounce, round(height, ndigits=4))
