04d78a6b076d1b41f59615b1934a45b477ffccbb
srschwartz <srschwartz@utexas.edu>
2023-07-19 18:56:24 -0500

    ex 1.10

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,19 +6,22 @@ rate = 0.05
 payment = 2684.11
 extra = 1000
 total_paid = 0.0
-month = 1
+month = 0
 extra_payment_start_month = 61
 extra_payment_end_month = 108
 
 while principal > 0:
+    month += 1
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
-    month += 1
-    while extra_payment_start_month <= month <= extra_payment_end_month:
-        principal = principal * (1 + rate / 12) - (payment + extra)
-        total_paid = total_paid + (payment + extra)
-        month += 1
+
+    if extra_payment_start_month <= month <= extra_payment_end_month:
+        principal = principal - extra
+        total_paid = total_paid + extra
+
+    print(month, total_paid, principal)
 
 print('Total paid', total_paid)
+print('Months', month)
 
 # The answer to Ex 1.9 is: Total paid 880074.0999999964
