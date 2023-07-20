e213dede0a06369dd6021911d7b7a5e5fc8ba3cb
srschwartz <srschwartz@utexas.edu>
2023-07-20 10:43:42 -0500

    ex 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,15 +1,29 @@
 # pcost.py
 #
 # Exercise 1.27
-total_cost = 0.0
 
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f)
-    for line in f:
-        row = line.split(',')
-        print(row)
-        shares = int(row[1])
-        price = float(row[2])
-        total_cost += shares * price
 
-print('Total cost is', round(total_cost, ndigits=2))
+# with open('Data/portfolio.csv', 'rt') as f:
+#     headers = next(f)
+#     for line in f:
+#         row = line.split(',')
+#         print(row)
+#         shares = int(row[1])
+#         price = float(row[2])
+#         total_cost += shares * price
+#
+# print('Total cost is', round(total_cost, ndigits=2))
+
+def portfolio_cost(filename):
+    total_cost = 0.0
+    with open(filename, 'rt') as f:
+        headers = next(f)
+        for line in f:
+            row = line.split(',')
+            shares = int(row[1])
+            price = float(row[2])
+            total_cost += round((shares * price), ndigits=2)
+    return total_cost
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost is', cost)
