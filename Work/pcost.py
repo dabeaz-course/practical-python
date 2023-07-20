b87de3286f4f691b01cb7ba7e94fc6884b0a0acd
srschwartz <srschwartz@utexas.edu>
2023-07-20 11:00:42 -0500

    ex 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -13,23 +13,25 @@
 #         total_cost += shares * price
 #
 # print('Total cost is', round(total_cost, ndigits=2))
+import csv
+
 
 def portfolio_cost(filename):
     total_cost = 0.0
     ctr = 0
     with open(filename, 'rt') as f:
-        headers = next(f)
-        for line in f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for line in rows:
             ctr += 1
-            row = line.split(',')
             try:
-                shares = int(row[1])
-                price = float(row[2])
+                shares = int(line[1])
+                price = float(line[2])
                 total_cost += shares * price
             except ValueError:
                 print('ValueError at row', ctr)
 
         return total_cost
 
-cost = portfolio_cost('Data/missing.csv')
+cost = portfolio_cost('Data/portfolio.csv')
 print('Total cost is', cost)
