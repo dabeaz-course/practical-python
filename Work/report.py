26efe913c7b5ddda5cd8fa242199a9e3ff106689
srschwartz <srschwartz@utexas.edu>
2023-08-01 16:47:40 -0500

    ex 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,24 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+import sys
+
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+        return portfolio
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+print(read_portfolio(filename))
