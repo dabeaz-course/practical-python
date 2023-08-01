cc1efc42bc8f5969f3e70c4807de69e605afdc5f
srschwartz <srschwartz@utexas.edu>
2023-08-01 17:39:24 -0500

    ex 2.7

--- Work/report.py
+++ Work/report.py
@@ -6,9 +6,10 @@ import csv
 import sys
 from pprint import pprint
 
+
 def read_portfolio(filename):
     portfolio = []
-    with open(filename, 'rt') as f:
+    with open(filename) as f:
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
@@ -17,10 +18,35 @@ def read_portfolio(filename):
         return portfolio
 
 
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-else:
-    filename = 'Data/portfolio.csv'
+def read_prices(filename):
+    prices = {}
+    with open(filename) as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+        return prices
 
-portfolio = read_portfolio(filename)
-pprint(portfolio)
+portfolio = read_portfolio('Data/portfolio.csv')
+prices = read_prices('Data/prices.csv')
+
+total_cost = 0.0
+for line in portfolio:
+    total_cost += line['shares'] * line['price']
+
+current_value = 0.0
+for line in portfolio:
+    n = line['name']
+    current_value += line['shares'] * prices[n]
+
+
+print('Original cost', total_cost)
+print('Current value', current_value)
+gain = current_value - total_cost
+print('Gain', gain)
+if gain > 0:
+    print('Hooray!')
+else:
+    print('Oh no')
