ddfc62fc5efab1ca4212c97ff672f597ddd96313
Kevin Reed <kevin.a.reed@gmail.com>
2023-08-01 22:32:06 -0500

    1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+
+def pcost():
+    with open('Data/portfolio.csv', 'rt') as f:
+        next(f) # skip header
+        cost = 0.0
+        for line in f:
+            row = line.split(',')
+            shares = int(row[1])
+            stock_cost = float(row[2])
+            cost += shares * stock_cost
+    return cost
+
+print(f'Total cost ${pcost():,.2f}')
---
+++ Work/pcost_test.py
@@ -0,0 +1,4 @@
+from pcost import pcost
+
+def test_pcost():
+    assert pcost() == 44671.15
