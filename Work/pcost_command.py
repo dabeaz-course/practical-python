7fefd9b295b1081bb9ce77f6d0edac61473d324e
Kevin Reed <kevin.a.reed@gmail.com>
2023-08-01 22:47:54 -0500

    1.33

--- Work/pcost_command.py
+++ Work/pcost_command.py
@@ -3,9 +3,10 @@
 from pcost import pcost
 import sys
 
-print(sys)
+print(sys.argv)
+print()
 
-if len(sys.argv) == 2:
+if len(sys.argv) > 1:
     filename = sys.argv[1]
 else:
     filename = 'Data/portfolio.csv'
