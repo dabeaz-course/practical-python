782eb58ad3981ed9d3db171b481a6cacda58673a
Kevin Reed <kevin.a.reed@gmail.com>
2023-08-14 22:21:21 -0500

    2.15

--- Work/pcost.py
+++ Work/pcost.py
@@ -7,13 +7,12 @@ def pcost(filename):
     '''Returns the total cost of the portfolio'''
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
-        next(rows) # skip header
         cost = 0.0
-        for fields in rows:
+        for line_no, row in enumerate(rows, start=1):
             try:
-                shares = int(fields[1])
-                stock_cost = float(fields[2])
+                shares = int(row[1])
+                stock_cost = float(row[2])
                 cost += shares * stock_cost
             except ValueError:
-                print(f'Could not parse {fields}')
+                print(f'Could not parse line {line_no} with data: {row}')
     return cost
