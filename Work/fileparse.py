8b8f323c080948a9bd4d393d760fabee34cab1cd
Kevin Reed <kevin.a.reed@gmail.com>
2023-08-15 23:23:11 -0500

    3.5

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,27 +4,30 @@
 
 import csv
 
-def parse_csv(filename, select: list):
-    '''
+
+def parse_csv(filename, select: list = [], types: list = []):
+    """
     Parse a CSV file into a list of records
-    '''
+    """
     with open(filename) as f:
         rows = csv.reader(f)
 
         # Read the file headers
-        headers = next(rows)
-        select_indices = [headers.index(name) for name in select] \
-            if select \
+        file_headers = next(rows)
+        headers = select if select else file_headers
+        indices = (
+            [headers.index(name) for name in select]
+            if select
             else [i for i in range(len(headers))]
-        if select:
-            headers = select
+        )
         records = []
         for row in rows:
-            if not row:    # Skip rows with no data
+            if not row:  # Skip rows with no data
                 continue
-            record = dict(zip(
-                headers,
-                [row[i] for i in select_indices]))
+            select_row = [row[col] for col in indices]
+            if types:
+                select_row = [func(col) for func, col in zip(types, select_row)]
+            record = dict(zip(headers, select_row))
             records.append(record)
 
     return records
--- Work/fileparse_test.py
+++ Work/fileparse_test.py
@@ -1,8 +1,37 @@
 from fileparse import parse_csv
 
+
 def test_parse_csv_select_3_4():
-    assert parse_csv('Data/portfolio.csv', select=['name','shares']) == \
-        [{'name': 'AA', 'shares': '100'}, {'name': 'IBM', 'shares': '50'},
-         {'name': 'CAT', 'shares': '150'}, {'name': 'MSFT', 'shares': '200'},
-         {'name': 'GE', 'shares': '95'}, {'name': 'MSFT', 'shares': '50'},
-         {'name': 'IBM', 'shares': '100'}]
+    assert parse_csv("Data/portfolio.csv", select=["name", "shares"]) == [
+        {"name": "AA", "shares": "100"},
+        {"name": "IBM", "shares": "50"},
+        {"name": "CAT", "shares": "150"},
+        {"name": "MSFT", "shares": "200"},
+        {"name": "GE", "shares": "95"},
+        {"name": "MSFT", "shares": "50"},
+        {"name": "IBM", "shares": "100"},
+    ]
+
+
+def test_parse_csv_select_3_5():
+    assert parse_csv("Data/portfolio.csv", types=[str, int, float]) == [
+        {"price": 32.2, "name": "AA", "shares": 100},
+        {"price": 91.1, "name": "IBM", "shares": 50},
+        {"price": 83.44, "name": "CAT", "shares": 150},
+        {"price": 51.23, "name": "MSFT", "shares": 200},
+        {"price": 40.37, "name": "GE", "shares": 95},
+        {"price": 65.1, "name": "MSFT", "shares": 50},
+        {"price": 70.44, "name": "IBM", "shares": 100},
+    ]
+
+    assert parse_csv(
+        "Data/portfolio.csv", select=["name", "shares"], types=[str, int]
+    ) == [
+        {"name": "AA", "shares": 100},
+        {"name": "IBM", "shares": 50},
+        {"name": "CAT", "shares": 150},
+        {"name": "MSFT", "shares": 200},
+        {"name": "GE", "shares": 95},
+        {"name": "MSFT", "shares": 50},
+        {"name": "IBM", "shares": 100},
+    ]
