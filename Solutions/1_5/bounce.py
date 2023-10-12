c380bd5e6c92d0cad43f956728a864a5adfaffed
Tekalign Mesfin <105485636+Tekalig@users.noreply.github.com>
2023-10-12 11:36:34 +0300

    Delete Solutions directory

--- Solutions/1_5/bounce.py
+++
@@ -1,8 +0,0 @@
-# bounce.py
-
-height = 100
-bounce = 1
-while bounce <= 10:
-    height = height * (3/5)
-    print(bounce, round(height, 4))
-    bounce += 1
--- Solutions/9_3/porty-app/print-report.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# print-report.py
-
-import sys
-from porty.report import main
-main(sys.argv)
